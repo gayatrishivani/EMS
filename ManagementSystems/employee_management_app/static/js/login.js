a2da069282bf575af0c90545cdb6a3fd6fc9c5be
Gayatri Shivani <33094218+gayatrishivani@users.noreply.github.com>
2022-03-16 16:41:08 +0530

    Delete ManagementSystems directory

--- ManagementSystems/employee_management_app/static/js/login.js
+++
@@ -1,8 +0,0 @@
-
-function validation(){
-    if(document.getElementById("exampleInputEmail1").value && document.getElementById("exampleInputPassword1").value){
-        document.getElementById("button").disabled = false;
-    }else{
-        document.getElementById("button").disabled = true;
-    }
-}
